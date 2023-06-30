c84f61d6d37afdad4f47b98ccd12c57930f45e5c
Ahmed Salem <aieisalem17@gmail.com>
2023-06-30 14:03:20 +0100

    Use camera rotation matrix to align the contours

--- warp/project.py
+++ warp/project.py
@@ -1,18 +1,73 @@
 import numpy as np
 
-def project_from_world(camera_pos, pts, focal_length, sensor_size):
+def rot_matrix_from_angles(x, y, z):
+    C = np.cos
+    S = np.sin
+
+    Rx     = [      
+        [1     , 0,     0     ], 
+        [0     , C(x),  -S(x) ], 
+        [0     , S(x),  C(x) ]]
+
+    Ry     = [      
+        [C(y)  , 0,     S(y)  ], 
+        [0     , 1,     0     ], 
+        [-S(y) , 0,     C(y)    ]]
+
+    Rz     = [      
+        [C(z)  , -S(z), 0     ], 
+        [S(z)  , C(z),  0     ], 
+        [0     , 0,     1     ]]
+
+
+    Rx = np.array(Rx)
+    Ry = np.array(Ry)
+    Rz = np.array(Rz)
+
+    final_rot = Rx.dot(Ry).dot(Rz)
+    return final_rot
+
+def project_from_world(camera_pos, camera_rot, pts):
     shape = pts.shape
     npts = pts.reshape((-1, 3)).copy()
-    npts = npts - camera_pos
-    
-    npts[:,2] = 1 / npts[:,2]
-    f = focal_length
-    proj_matrix = np.array([
-        [f, 0, 0],
-        [0, f, 0],
-        [1, 1, 0]
-    ])
 
-    npts = npts.dot(proj_matrix)
+    npts = npts - camera_pos
+    R = rot_matrix_from_angles(*camera_rot)
+    npts = npts.dot(R)
+    
     return npts.reshape(shape)
+    
+
+if __name__ == "__main__":
+    import cv2
+    
+    camera_pos = np.array([100, 100, 100])
+    camera_rot = [np.pi/4, np.pi/4, np.pi/4]
+    
+    focal_length = 1
+    sensor_size = 100
+    
+    canvas = np.ones((200, 200, 3), np.uint8) * 70
+
+    contours = np.array([[50, 50, 100], [50, 100, 100], [100,100, 100],[100, 50, 100]])
+    contours_proj = project_from_world(camera_pos, camera_rot, contours)
+    print(contours)
+    print(contours_proj)
+    pts = contours[:, [0,1]].astype(np.int32)
+    pts_proj = contours_proj[:,[0,1]].astype(np.int32)
+
+    print(pts_proj)
+
+
+    canvas_cam = np.ones((200, 200, 3), np.uint8) * 200
+    
+    pts = pts.reshape(-1, 1, 2)
+    pts_proj = pts_proj.reshape(-1, 1, 2) + np.array([100, 100])
+
+    
+    cv2.drawContours(canvas, [pts], -1, (20, 20, 200), -1)
+    cv2.drawContours(canvas_cam, [pts_proj], -1, (200, 20, 20), -1)
+    
+    cv2.imshow("canvas", np.hstack((canvas, canvas_cam)))
+    cv2.waitKey(0)
     
--- warp/simple_warp.py
+++ warp/simple_warp.py
@@ -3,6 +3,8 @@ import numpy as np
 import math
 import random
 
+from project import *
+
 def rot_vec(vec, theta):
     C = np.cos(theta)
     S = np.sin(theta)
@@ -42,7 +44,7 @@ def make_matrix(r, h, mw, mh):
     return alpha, M
 
 
-def rotate_im(h, w):
+def rotate_im(h, w, cam_x, cam_y, cam_z, cam_rot_x, cam_rot_y, cam_rot_z, focal_length, sensor_size):
     H = 2160
     
     mw = w // 2
@@ -53,31 +55,50 @@ def rotate_im(h, w):
 
     polygons = []
     theta = 0
-    
+
+    polygon_pts = []
     while TH < 2000:
         alpha, M = make_matrix(r, TH, mw, mh)
         theta += alpha
         R = rot_mat(theta)
         M_p = M.dot(R)
         polygons.append((circle_norm(theta), M_p, M_p[0][-1]))
-
+        polygon_pts.append(M_p)
+        
         n_iter = int(2*np.pi / alpha) - 1
         TH += h / n_iter 
         r += 10 / (n_iter)
         
     canvas = np.ones((H, 1000, 3), np.uint8) * 200
     canvas_top = canvas.copy()
+    canvas_cam = canvas.copy()
+    
 
     H, W = canvas.shape[:2]
 
     HI = 50
     WI = W // 2
 
-    print(len(polygons))
+    deg2rad = lambda x: x * np.pi / 180
+    camera_pos = np.array((cam_x, cam_y, cam_z))
+    camera_rot = [deg2rad(x) for x in [cam_rot_x, cam_rot_y, cam_rot_z]]
+    polygon_pts = np.array(polygon_pts)
+    polygon_pts = project_from_world(camera_pos, camera_rot, polygon_pts)
+    #polygon_pts = polygon_pts.reshape(-1, 4, 1, 2).astype(np.int32)
+    #print(polygon_pts.shape)
+    #polygon_pts = [ p for p in polygon_pts]
+    #cv2.drawContours(canvas_cam, polygon_pts, -1, (20, 20, 140), -1)
+    #print(len(polygons))
+
     polygons.sort(key=lambda k: k[2])
 
+
+    canvas_r = np.ones((H*2, W*2, 3), np.uint8) * 200
+    canvas_r[:H,:W] = canvas
+    canvas_r[:H,W:] = canvas_top
+    canvas_r[H:,:W] = canvas_cam
     
-    for theta, polygon, _ in polygons:
+    for i, (theta, polygon, _) in enumerate(polygons):
         if abs(theta - np.pi) <= np.pi/2:
             c = (40, 40, 40)
         else:
@@ -85,27 +106,37 @@ def rotate_im(h, w):
             
         pts = list(map(lambda pt: (WI + pt[0], HI + pt[1]), polygon))
         pts = np.array(pts, np.int32).reshape((-1, 1, 2))
+        
         cv2.drawContours(canvas, [pts], 0, c, -1)
 
         tr, tl = list(map(lambda pt: (WI + int(pt[0]), WI + int(pt[2])), polygon[:2]))
         cv2.line(canvas_top, tr, tl, c, 2)
 
-        disp = cv2.resize(np.hstack((canvas, canvas_top)), None, fx=0.5, fy=0.5)
-        yield disp
-    #     cv2.imshow("Canvas", disp)
-    #     cv2.waitKey(1)
+        polygon = polygon_pts[i]
+        pts = list(map(lambda pt: (WI + pt[0], (H // 2) + pt[1]), polygon))
+        pts = np.array(pts, np.int32).reshape((-1, 1, 2))
+        
+        cv2.drawContours(canvas_cam, [pts], 0, c, -1)
 
+        canvas_r[:H,:W] = canvas
+        canvas_r[:H,W:] = canvas_top
+        canvas_r[H:,:W] = canvas_cam
 
-    # disp = cv2.resize(np.hstack((canvas, canvas_top)), None, fx=0.5, fy=0.5)
-    # cv2.imshow("Canvas", disp)
-    # cv2.waitKey(0)
-
+        yield canvas_r
 
 def create_demo():
     import gradio as gr
-    rotate_im(80, 80)
-
-    demo = gr.Interface(rotate_im, [gr.Slider(minimum = 0, maximum = 200, value=80), gr.Slider(minimum = 0, maximum = 200, value=80)], gr.Image())
+    demo = gr.Interface(rotate_im, [gr.Slider(minimum = 0, maximum = 400, value=400),
+                                    gr.Slider(minimum = 0, maximum = 200, value=80),
+                                    gr.Slider(minimum = 0, maximum = 2000, value=500),
+                                    gr.Slider(minimum = 0, maximum = 2000, value=500),
+                                    gr.Slider(minimum = 0, maximum = 2000, value=1000),
+                                    gr.Slider(minimum = 0, maximum = 360, value=0),
+                                    gr.Slider(minimum = 0, maximum = 360, value=0),
+                                    gr.Slider(minimum = 0, maximum = 360, value=0),
+                                    gr.Slider(minimum = 0, maximum = 20, value=2),
+                                    gr.Slider(minimum = 0, maximum = 100, value=30),
+                                    ], gr.Image())
     demo.queue()
     demo.launch()
     
